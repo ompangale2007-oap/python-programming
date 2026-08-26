ef0402ca18a1ac2eadb3dc26ec4dc12434146a72
ompangale2007-oap <ompangale2007@gmail.com>
2026-08-26 13:10:16 +0530

    Add files via upload

---
+++ BASICS/list2.py
@@ -0,0 +1,9 @@
+L=[
+    input("Enter marks of 1st student : "),
+     input("Enter marks of 2nd student : "),
+      input("Enter marks of 3rd student : "),
+       input("Enter marks of 4th student : "),
+        input("Enter marks of 5th student : "),
+]
+L.sort()
+print("Sorted list is :",L)
