380fa1ed874a40df550373ac8122eba8312ef85b
ompangale2007-oap <ompangale2007@gmail.com>
2026-08-30 17:11:07 +0530

    Add files via upload

---
+++ BASICS/mark.py
@@ -0,0 +1,8 @@
+a=int(input("Enter marks of 1st subject : "))
+b=int(input("Enter marks of 2nd subject : "))
+c=int(input("Enter marks of 3rd subject : "))
+Total=a+b+c
+if Total>=35:
+    print("Pass")
+else:
+    print("Fail")
