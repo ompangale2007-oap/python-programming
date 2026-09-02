a33175d94fbaddb78eec6cecd727942f49a6e72f
ompangale2007-oap <ompangale2007@gmail.com>
2026-09-02 14:00:36 +0530

    Add files via upload

---
+++ BASICS/ifelse2.py
@@ -0,0 +1,6 @@
+A = ["Akash","Ganesh","Rohan","AAA","BBB"]
+name=input("Enter name for search : ")
+if name in A:
+    print(f"{name} is present in given list ")
+else:
+     print(f"{name}is not present in given list ")
