eb1fb0d28a462d7ae24207124482afbb9fc7244e
ompangale2007-oap <ompangale2007@gmail.com>
2026-09-05 23:55:36 +0530

    Add files via upload

---
+++ BASICS/dict2.py
@@ -0,0 +1,11 @@
+Language = {}
+n1 = input("n1's name : ")
+Language[n1] = input(f"{n1}'s fav language : ")
+n2 = input("n2's name : ")
+Language[n2] = input(f"{n2}'s fav language : ")
+n3 = input("n3's name : ")
+Language[n3] = input(f"{n3}'s fav language : ")
+n4 = input("n4's name : ")
+Language[n4] = input(f"{n4}'s fav language : ")
+print(Language)
+
